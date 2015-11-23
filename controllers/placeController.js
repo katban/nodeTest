f61aed045297af212012a6b1846d03f71ae7f388
katban <grancen@wp.pl>
2015-11-23 13:55:55 +0100

    controllers

---
+++ controllers/placeController.js
@@ -0,0 +1,94 @@
+/**
+ * Created by katban on 23.11.15.
+ */
+
+var Place = require('../models/place.js');
+
+exports.getPlaces = function (reg, res) {
+    Place.find(function(err, places){
+        if (err) {
+            res.send(err);
+        }
+        res.json(places);
+    });
+};
+
+
+// sciezka do posta
+exports.postPlaces = function (req, res) {
+    // nowa instancja danego obiektu
+    var place = new Place();
+
+    place.name = req.body.name;
+    place.city = req.body.city;
+    place.buildingName = req.body.buildingName;
+    place.street = req.body.street;
+    place.buildingNumber = req.body.buildingNumber;
+    place.phone = req.body.phone;
+
+    //zapis do bazy
+    place.save(function(err) {
+        if (err) {
+            // jesli jest error to ma wysłać go do uzytkownika
+            res.send(err);
+        }
+        res.json({message: 'Place added to database', data: place});
+    });
+};
+
+
+
+exports.getPlace = function(req, res) {
+    Place.findById(req.params.place_id, function(err, place) {
+        if(err) {
+            res.send(err);
+        }
+        res.json(place);
+    });
+};
+
+
+//put
+exports.putPlace = function(req, res) {
+    Place.findById(req.params.place_id, function(err, place) {
+        if(err) {
+            res.send(err);
+        }
+
+        if(req.body.name) {
+            place.name = req.body.name;
+        }
+        if(req.body.city) {
+            place.city = req.body.city;
+        }
+        if(req.body.buildingName) {
+            place.buildingName = req.body.buildingName;
+        }
+        if(req.body.street) {
+            place.street = req.body.street;
+        }
+        if(req.body.buildingNumber) {
+            place.buildingNumber = req.body.buildingNumber;
+        }
+        if(req.body.phone) {
+            place.phone = req.body.phone;
+        }
+
+
+        place.save(function (err) {
+            if(err) {
+                res.send(err);
+            }
+            res.json(place);
+        })
+
+    })
+};
+
+exports.deletePlace = function(req,res){
+    Place.findByIdRemove(req.params.plac_id, function(err) {
+        if(err) {
+            res.send(err);
+        }
+    });
+};
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 var Place = require('./models/place');  //bez .js
+var PlaceController = require('./controllers/placeController');
 
 //polaczenie z baza
 mongoose.connect('mongodb://localhost:27017/nodetest');
@@ -20,59 +21,24 @@ var port = 3000;
 // w ekspersie zawarty jest router - odpowiada za endpointy
 var router = express.Router();
 
-var placesRoute = router.route('/places');
+router.route('/places')
+	.post(PlaceController.postPlaces)
+	.get(PlaceController.getPlaces);
+
+
+router.route('/places/:place_id')
+	.get(PlaceController.getPlace)
+	.put(PlaceController.putPlace)
+	.delete(PlaceController.deletePlace);
+
 
 //pierwszy end-point
-placesRoute.get(function (reg, res) {
-	Place.find(function(err, places){
-		if (err) {
-			res.send(err);
-		}
-		res.json(places);
-	});
-});
-
-
-// sciezka do posta
-placesRoute.post(function (req, res) {
-	// nowa instancja danego obiektu
-	var place = new Place();
-
-	place.name = req.body.name;
-	place.city = req.body.city;
-	place.buildingName = req.body.buildingName;
-	place.street = req.body.street;
-	place.buildingNumber = req.body.buildingNymber;
-	place.phone = req.body.phone;
-
-	//zapis do bazy
-	place.save(function(err) {
-		if (err) {
-			// jesli jest error to ma wysłać go do uzytkownika
-			res.send(err);
-		}
-		res.json({message: 'Place added to database', data: place});
-	});
-});
-
 //drugi endpiont
 router.get('kwiatki', function(reg, res) {
 	res.json({
 		'name': 'Bratek'
 	});
 });
-
-var placeRoute = router.route('/places/:place_id');
-
-placeRoute.get(function(req, res) {
-	Place.findById(req.params.place_id, function(err, place) {
-		if(err) {
-			res.send(err);
-		}
-		res.json(place);
-	});
-});
-
 
 // router trzeba zarejestrowac w aplikacji
 app.use('/api', router);
